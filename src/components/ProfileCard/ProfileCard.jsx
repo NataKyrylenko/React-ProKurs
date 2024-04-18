77f6a9cd2b119befafac6124f4f8b0028e335ef4
NataKyrylenko <natalik10107919@gmail.com>
2024-04-18 10:31:13 +0200

    value correction

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,15 +1,19 @@
 import "./styles.css";
 
 function ProfileCard() {
-  const userName = "Alex Parker";
-  const userProfession = "Software Engineer specializing in artificial intelligence and robotics";
-  const userHobby = "Alex is passionate about creating autonomous robots for robotics competitions. In his spare time, he designs and programs new algorithms to enhance the performance and efficiency of robots. He also enjoys virtual reality and game development for VR platforms, where he can bring his ideas and creative concepts to life.";
+  const userData = {
+    avatar: "https://img.freepik.com/premium-vector/student-avatar-illustration-user-profile-icon-youth-avatar_118339-4406.jpg?w=740",
+    userName: "Alex Parker",
+    profession: "Software Engineer specializing in artificial intelligence and robotics",
+    hobbies: ["Reading ", "creating autonomous robots ", "Traveling "],
+  };
+  
   return (
     <div className="profileCard">
-      <img src="https://img.freepik.com/premium-vector/student-avatar-illustration-user-profile-icon-youth-avatar_118339-4406.jpg?w=740" alt="" />
-      <h3 className="userTitle">{userName}</h3>
-      <p className="userPretitle">{userProfession}</p>
-      <p>{userHobby}</p>
+      <img src={userData.avatar}/>
+      <h3 className="userTitle">{userData.userName}</h3>
+      <p className="userPretitle">{userData.profession}</p>
+      <p>{userData.hobbies[0]}</p>
     </div>
   );
 }
